f32ad052c4e053317b11b41531866467a00e6768
Delta6626 <hassan911ee@gmail.com>
2026-07-12 01:09:18 +0400

    fix: update HomeScreen layout to include topBar styles and reposition IconButton

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,5 +1,5 @@
 import { ScrollView, StyleSheet, View } from "react-native";
-import { Text } from "react-native-paper";
+import { IconButton, Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAppTheme } from "../../utils/useAppTheme";
 
@@ -13,13 +13,20 @@ export default function HomeScreen() {
       paddingVertical: theme.spacing.xl,
       paddingHorizontal: theme.spacing.xl,
     },
+
+    topBar: {
+      justifyContent: "space-between",
+      alignItems: "center",
+      flexDirection: "row",
+    },
   });
 
   return (
     <SafeAreaView style={styles.parentContainer}>
       <ScrollView>
-        <View>
+        <View style={styles.topBar}>
           <Text variant={"headlineSmall"}>LifeLog</Text>
+          <IconButton icon={"magnify"} mode={"contained-tonal"}></IconButton>
         </View>
       </ScrollView>
     </SafeAreaView>
