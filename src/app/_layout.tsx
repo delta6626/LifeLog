501ae664365e178928383c8f115baae6a68dc3cb
Delta6626 <hassan911ee@gmail.com>
2026-07-11 22:10:28 +0400

    chore: define screens

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,8 +3,15 @@ import { PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   return (
+    // Wrap with PaperProvider to apply material theming globally
     <PaperProvider>
-      <Stack />
+      {/* Define screens */}
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="home" />
+        <Stack.Screen name="entries" />
+        <Stack.Screen name="search" />
+        <Stack.Screen name="favorites" />
+      </Stack>
     </PaperProvider>
   );
 }
