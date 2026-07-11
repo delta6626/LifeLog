68f5ba436f125dcb908435c3d4c6f5b1cb618a26
Delta6626 <hassan911ee@gmail.com>
2026-07-11 22:57:31 +0400

    feat: update customAppTheme to include spacing for material theming

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,19 +1,23 @@
 import { Stack } from "expo-router";
 import { MD3LightTheme, PaperProvider } from "react-native-paper";
 
-// const customAppTheme = {
-//   ...MD3LightTheme,
-//   colors: {
-//     ...MD3LightTheme.colors,
-//     primary: "",
-//     secondary: "",
-//   },
-// };
+const customAppTheme = {
+  ...MD3LightTheme,
+  spacing: {
+    xs: 4,
+    sm: 8,
+    md: 12,
+    lg: 16,
+    xl: 24,
+    x2l: 32,
+    x3l: 48,
+  },
+};
 
 export default function RootLayout() {
   return (
     // Wrap with PaperProvider to apply material theming globally
-    <PaperProvider theme={MD3LightTheme}>
+    <PaperProvider theme={customAppTheme}>
       {/* Define screens */}
       <Stack screenOptions={{ headerShown: false }}>
         <Stack.Screen name="home" />
