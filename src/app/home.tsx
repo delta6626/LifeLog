b46be7e6b7b5463b50d9b92adf6be4a72b8a03fd
Delta6626 <hassan911ee@gmail.com>
2026-07-11 23:35:52 +0400

    refactor: streamline HomeScreen styles and update Text variant

--- src/app/home.tsx
+++ src/app/home.tsx
@@ -1,30 +1,25 @@
-import { ScrollView, StyleSheet, Text } from "react-native";
+import { ScrollView, StyleSheet } from "react-native";
+import { Text } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useAppTheme } from "../../utils/useAppTheme";
 
 export default function HomeScreen() {
   const theme = useAppTheme();
 
+  const styles = StyleSheet.create({
+    parentContainer: {
+      flex: 1,
+      backgroundColor: theme.colors.background,
+      paddingVertical: theme.spacing.xl,
+      paddingHorizontal: theme.spacing.xl,
+    },
+  });
+
   return (
-    <SafeAreaView
-      style={[
-        styles.container,
-        {
-          backgroundColor: theme.colors.background,
-          paddingVertical: theme.spacing.xl,
-          paddingHorizontal: theme.spacing.xl,
-        },
-      ]}
-    >
+    <SafeAreaView style={styles.parentContainer}>
       <ScrollView>
-        <Text>LifeLog</Text>
+        <Text variant={"headlineSmall"}>LifeLog</Text>
       </ScrollView>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
