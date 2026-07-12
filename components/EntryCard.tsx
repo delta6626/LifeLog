1744c2883572f52cb339d12893239c0c4de39f21
Delta6626 <hassan911ee@gmail.com>
2026-07-12 22:00:54 +0400

    feat: add EntryCard component for user entries with styled layout and interaction buttons

---
+++ components/EntryCard.tsx
@@ -0,0 +1,76 @@
+import { StyleSheet, View } from "react-native";
+import { IconButton, Text, TouchableRipple } from "react-native-paper";
+import { useAppTheme } from "../utils/useAppTheme";
+
+export const EntryCard = () => {
+  const theme = useAppTheme();
+
+  const styles = StyleSheet.create({
+    parentContainer: {
+      flex: 1,
+      padding: theme.spacing.md,
+      backgroundColor: theme.colors.surface,
+      borderRadius: theme.radii.xl,
+      borderWidth: 1,
+      borderColor: theme.colors.outline,
+    },
+
+    titleText: {
+      color: theme.colors.primary,
+    },
+
+    bodyText: {
+      color: theme.colors.onSurfaceVariant,
+      marginTop: theme.spacing.xs,
+    },
+
+    bottomContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-between",
+      marginTop: theme.spacing.xs,
+    },
+
+    dateText: {
+      color: theme.colors.muted,
+    },
+
+    iconButtonsContainer: {
+      flexDirection: "row",
+    },
+  });
+
+  return (
+    <TouchableRipple style={styles.parentContainer}>
+      <View>
+        <Text variant={"titleMedium"} style={styles.titleText}>
+          Wrapping up a lonk week
+        </Text>
+        <Text variant={"bodyMedium"} style={styles.bodyText}>
+          Closed my laptop a little early tonight. It's the first time in weeks
+          I didn't feel behind on something.
+        </Text>
+
+        <View style={styles.bottomContainer}>
+          <Text variant={"bodySmall"} style={styles.dateText}>
+            10 July, 11:33 PM
+          </Text>
+          <View style={styles.iconButtonsContainer}>
+            <IconButton
+              mode={"outlined"}
+              size={16}
+              icon={"delete-outline"}
+              iconColor={theme.colors.muted}
+            />
+            <IconButton
+              mode={"outlined"}
+              size={16}
+              icon={"heart-outline"}
+              iconColor={theme.colors.muted}
+            />
+          </View>
+        </View>
+      </View>
+    </TouchableRipple>
+  );
+};
