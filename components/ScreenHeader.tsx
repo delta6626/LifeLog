274aa9424d02b10296d70c084035e2460440aeee
Delta6626 <hassan911ee@gmail.com>
2026-07-13 01:31:27 +0400

    feat: enhance ScreenHeader component with back navigation and styling

--- components/ScreenHeader.tsx
+++ components/ScreenHeader.tsx
@@ -1,4 +1,5 @@
-import { View } from "react-native";
+import { router } from "expo-router";
+import { StyleSheet, View } from "react-native";
 import { IconButton, Text } from "react-native-paper";
 import { ScreenHeaderTitle } from "../types/ScreenHeaderName";
 import { useAppTheme } from "../utils/useAppTheme";
@@ -10,13 +11,39 @@ interface ScreenHeaderProps {
 export const ScreenHeader = ({ screenHeaderTitle }: ScreenHeaderProps) => {
   const theme = useAppTheme();
 
+  const styles = StyleSheet.create({
+    parentContainer: {
+      flexDirection: "row",
+      alignItems: "center",
+      gap: theme.spacing.sm,
+    },
+
+    titleText: {
+      color: theme.colors.onPrimaryContainer,
+    },
+  });
+
+  const handleBackButtonPress = () => {
+    router.back();
+  };
+
   return (
-    <View>
+    <View style={styles.parentContainer}>
+      {/* Back button to go to the previous screen */}
       <IconButton
+        mode={"contained-tonal"}
+        size={20}
+        containerColor={theme.colors.primaryContainer}
         icon={"arrow-left"}
         iconColor={theme.colors.onPrimaryContainer}
+        onPress={handleBackButtonPress}
       ></IconButton>
-      <Text variant={"titleMedium"}>{screenHeaderTitle}</Text>
+
+      <View>
+        <Text variant={"titleMedium"} style={styles.titleText}>
+          {screenHeaderTitle}
+        </Text>
+      </View>
     </View>
   );
 };
