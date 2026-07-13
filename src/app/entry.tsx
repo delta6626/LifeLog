fbbc67b86eab30abf2fecc3cd1734551209981a0
Delta6626 <hassan911ee@gmail.com>
2026-07-13 22:05:46 +0400

    feat: enhance EntryScreen with rich text editor and toolbar integration

--- src/app/entry.tsx
+++ src/app/entry.tsx
@@ -1,4 +1,5 @@
-import { ScrollView, StyleSheet } from "react-native";
+import { RichText, Toolbar, useEditorBridge } from "@10play/tentap-editor";
+import { KeyboardAvoidingView, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScreenHeader } from "../../components/ScreenHeader";
 import { useAppTheme } from "../../utils/useAppTheme";
@@ -13,13 +14,52 @@ export default function EntryScreen() {
       paddingVertical: theme.spacing.xl,
       paddingHorizontal: theme.spacing.xl,
     },
+
+    toolbarView: {
+      height: 40,
+    },
+  });
+
+  const editor = useEditorBridge({
+    autofocus: true,
+    avoidIosKeyboard: true,
+    initialContent: "Start editing!",
+    theme: {
+      toolbar: {
+        toolbarBody: {
+          borderRadius: theme.radii.x3l,
+          backgroundColor: theme.colors.outline,
+          borderTopWidth: 0,
+          borderBottomWidth: 0,
+          borderLeftWidth: 0,
+          borderRightWidth: 0,
+        },
+
+        toolbarButton: {
+          backgroundColor: theme.colors.outline,
+        },
+
+        iconWrapper: {
+          backgroundColor: theme.colors.outline,
+          borderRadius: theme.radii.x3l,
+        },
+      },
+
+      webview: {
+        backgroundColor: "transparent",
+      },
+    },
   });
 
   return (
     <SafeAreaView style={styles.parentContainer}>
-      <ScrollView>
-        <ScreenHeader screenHeaderTitle={"View memory"} />
-      </ScrollView>
+      <ScreenHeader screenHeaderTitle={"View memory"} />
+      <View style={styles.toolbarView}>
+        <Toolbar editor={editor} hidden={false} />
+      </View>
+      <KeyboardAvoidingView style={{ flex: 1 }}>
+        <RichText editor={editor} />
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
