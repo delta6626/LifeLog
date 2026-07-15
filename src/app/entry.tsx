162a036725ef88f7d3058dc7cffb7801151b4b33
Delta6626 <hassan911ee@gmail.com>
2026-07-15 20:20:16 +0400

    feat: enhance EntryScreen to support dynamic titles and content based on entry mode

--- src/app/entry.tsx
+++ src/app/entry.tsx
@@ -2,36 +2,20 @@ import { RichText, Toolbar, useEditorBridge } from "@10play/tentap-editor";
 import { KeyboardAvoidingView, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScreenHeader } from "../../components/ScreenHeader";
+import { useCurrentEntryStore } from "../../store/currentEntryStore";
+import { useEntryScreenModeStore } from "../../store/entryScreenModeStore";
 import { useAppTheme } from "../../utils/useAppTheme";
 
 export default function EntryScreen() {
   const theme = useAppTheme();
 
-  const styles = StyleSheet.create({
-    parentContainer: {
-      flex: 1,
-      backgroundColor: theme.colors.background,
-      paddingVertical: theme.spacing.xl,
-      paddingHorizontal: theme.spacing.xl,
-    },
-
-    editor: {
-      flex: 1,
-      backgroundColor: "transparent",
-    },
-
-    toolbarContainer: {
-      position: "absolute",
-      left: theme.spacing.xl,
-      right: theme.spacing.xl,
-      bottom: theme.spacing.xl,
-    },
-  });
+  const { currentEntry } = useCurrentEntryStore();
+  const { entryScreenMode } = useEntryScreenModeStore();
 
   const editor = useEditorBridge({
-    autofocus: true,
+    autofocus: entryScreenMode === "read" ? false : true,
     avoidIosKeyboard: true,
-    initialContent: "Start editing!",
+    initialContent: currentEntry?.content,
     theme: {
       toolbar: {
         toolbarBody: {
@@ -60,9 +44,38 @@ export default function EntryScreen() {
     },
   });
 
+  const styles = StyleSheet.create({
+    parentContainer: {
+      flex: 1,
+      backgroundColor: theme.colors.background,
+      paddingVertical: theme.spacing.xl,
+      paddingHorizontal: theme.spacing.xl,
+    },
+
+    editor: {
+      flex: 1,
+      backgroundColor: "transparent",
+    },
+
+    toolbarContainer: {
+      position: "absolute",
+      left: theme.spacing.xl,
+      right: theme.spacing.xl,
+      bottom: theme.spacing.xl,
+    },
+  });
+
   return (
     <SafeAreaView style={styles.parentContainer}>
-      <ScreenHeader screenHeaderTitle="View memory" />
+      <ScreenHeader
+        screenHeaderTitle={
+          entryScreenMode === "create"
+            ? "New memory"
+            : entryScreenMode === "edit"
+              ? "Edit memory"
+              : "View memory"
+        }
+      />
 
       <RichText
         editor={editor}
@@ -73,7 +86,10 @@ export default function EntryScreen() {
         behavior={"padding"}
         style={styles.toolbarContainer}
       >
-        <Toolbar editor={editor} hidden={false} />
+        <Toolbar
+          editor={editor}
+          hidden={entryScreenMode === "read" ? true : false}
+        />
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
