9144549cb0f89d03194cf757a170ebef3a552cb4
Delta6626 <hassan911ee@gmail.com>
2026-07-16 21:54:49 +0400

    feat: enhance DeleteEntryModal store with idForDeletion and setter method

--- store/deleteEntryModalStore.ts
+++ store/deleteEntryModalStore.ts
@@ -1,13 +1,18 @@
 import { create } from "zustand";
+import { Entry } from "../types/Entry";
 
 interface DeleteEntryModalStore {
   isVisible: boolean;
+  idForDeletion: Entry["id"] | null;
   setIsVisible: (isVisible: boolean) => void;
+  setIdForDeletion: (id: Entry["id"]) => void;
 }
 
 export const useDeleteEntryModalStore = create<DeleteEntryModalStore>(
   (set) => ({
     isVisible: false,
     setIsVisible: (isVisible) => set({ isVisible: isVisible }),
+    idForDeletion: null,
+    setIdForDeletion: (idForDeletion) => set({ idForDeletion: idForDeletion }),
   }),
 );
