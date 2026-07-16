051f23cb0d06efb4b6addedad893a29cb0b509d2
Delta6626 <hassan911ee@gmail.com>
2026-07-16 22:20:07 +0400

    feat: enhance DeleteEntryModal with improved layout and button styles

--- components/DeleteEntryModal.tsx
+++ components/DeleteEntryModal.tsx
@@ -11,9 +11,34 @@ export const DeleteEntryModal = () => {
     modal: {
       alignSelf: "center",
       width: "80%",
+      alignItems: "center",
       backgroundColor: theme.colors.background,
       padding: 24,
       borderRadius: theme.radii.xl,
+    },
+
+    icon: {
+      margin: 0,
+      backgroundColor: theme.colors.errorContainer,
+    },
+
+    title: {
+      marginTop: theme.spacing.md,
+    },
+
+    subtitle: {
+      textAlign: "center",
+      marginBottom: theme.spacing.xl,
+    },
+
+    cancelButton: {
+      width: "100%",
+      marginBottom: theme.spacing.sm,
+    },
+
+    deleteButton: {
+      width: "100%",
+      backgroundColor: theme.colors.errorContainer,
     },
   });
 
@@ -24,14 +49,29 @@ export const DeleteEntryModal = () => {
         visible={isVisible}
         dismissable={true}
       >
-        <IconButton icon={"delete-outline"} />
-        <Text variant={"titleMedium"}>Delete this memory?</Text>
-        <Text variant={"bodyMedium"}>
+        <IconButton
+          icon={"delete-outline"}
+          mode={"contained"}
+          style={styles.icon}
+          iconColor={theme.colors.error}
+        />
+        <Text variant={"titleMedium"} style={styles.title}>
+          Delete this memory?
+        </Text>
+        <Text variant={"bodyMedium"} style={styles.subtitle}>
           This memory will be permanently deleted. This can't be undone.
         </Text>
 
-        <Button mode={"outlined"}>Cancel</Button>
-        <Button mode={"contained"}>Delete</Button>
+        <Button mode={"contained"} style={styles.cancelButton}>
+          Cancel
+        </Button>
+        <Button
+          mode={"contained"}
+          style={styles.deleteButton}
+          textColor={theme.colors.error}
+        >
+          Delete
+        </Button>
       </Modal>
     </Portal>
   );
