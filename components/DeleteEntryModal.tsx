428f6f8d827b9a2695decb3e6433420218b81b41
Delta6626 <hassan911ee@gmail.com>
2026-07-16 22:11:12 +0400

    feat: enhance DeleteEntryModal with improved styling and confirmation text

--- components/DeleteEntryModal.tsx
+++ components/DeleteEntryModal.tsx
@@ -1,13 +1,37 @@
-import { IconButton, Modal, Portal } from "react-native-paper";
+import { StyleSheet } from "react-native";
+import { Button, IconButton, Modal, Portal, Text } from "react-native-paper";
 import { useDeleteEntryModalStore } from "../store/deleteEntryModalStore";
+import { useAppTheme } from "../utils/useAppTheme";
 
 export const DeleteEntryModal = () => {
   const { isVisible } = useDeleteEntryModalStore();
+  const theme = useAppTheme();
+
+  const styles = StyleSheet.create({
+    modal: {
+      alignSelf: "center",
+      width: "80%",
+      backgroundColor: theme.colors.background,
+      padding: 24,
+      borderRadius: theme.radii.xl,
+    },
+  });
 
   return (
     <Portal>
-      <Modal visible={isVisible}>
+      <Modal
+        contentContainerStyle={styles.modal}
+        visible={isVisible}
+        dismissable={true}
+      >
         <IconButton icon={"delete-outline"} />
+        <Text variant={"titleMedium"}>Delete this memory?</Text>
+        <Text variant={"bodyMedium"}>
+          This memory will be permanently deleted. This can't be undone.
+        </Text>
+
+        <Button mode={"outlined"}>Cancel</Button>
+        <Button mode={"contained"}>Delete</Button>
       </Modal>
     </Portal>
   );
