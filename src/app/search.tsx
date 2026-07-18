33d561b797366aaaf14b560e78d6fb8d6d2d77f9
Delta6626 <hassan911ee@gmail.com>
2026-07-18 21:18:18 +0400

    feat: enhance SearchScreen layout and add TextInput for title search

--- src/app/search.tsx
+++ src/app/search.tsx
@@ -1,4 +1,5 @@
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { TextInput } from "react-native-paper";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScreenHeader } from "../../components/ScreenHeader";
 import { useAppTheme } from "../../utils/useAppTheme";
@@ -10,6 +11,9 @@ export default function SearchScreen() {
     parentContainer: {
       flex: 1,
       backgroundColor: theme.colors.background,
+    },
+
+    childContainer: {
       paddingVertical: theme.spacing.xl,
       paddingHorizontal: theme.spacing.xl,
     },
@@ -18,7 +22,21 @@ export default function SearchScreen() {
   return (
     <SafeAreaView style={styles.parentContainer}>
       <ScrollView>
-        <ScreenHeader screenHeaderTitle={"Search memories"} />
+        <View style={styles.childContainer}>
+          <ScreenHeader screenHeaderTitle={"Search memories"} />
+
+          <View>
+            <TextInput
+              outlineStyle={{
+                borderRadius: theme.radii.full,
+              }}
+              mode={"outlined"}
+              multiline={false}
+              autoFocus={true}
+              placeholder={"Search by title"}
+            />
+          </View>
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
