296e4916584fdff70ef85b2e28ca7ece1673882d
Delta6626 <hassan911ee@gmail.com>
2026-07-19 22:11:11 +0400

    feat: restructure FavoritesScreen layout for better organization and readability

--- src/app/favorites.tsx
+++ src/app/favorites.tsx
@@ -1,15 +1,20 @@
-import { ScrollView, StyleSheet } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScreenHeader } from "../../components/ScreenHeader";
+import { useMetaDataStore } from "../../store/metaDataStore";
 import { useAppTheme } from "../../utils/useAppTheme";
 
 export default function FavoritesScreen() {
   const theme = useAppTheme();
+  const { metaDataList } = useMetaDataStore();
 
   const styles = StyleSheet.create({
     parentContainer: {
       flex: 1,
       backgroundColor: theme.colors.background,
+    },
+
+    childContainer: {
       paddingVertical: theme.spacing.xl,
       paddingHorizontal: theme.spacing.xl,
     },
@@ -18,7 +23,9 @@ export default function FavoritesScreen() {
   return (
     <SafeAreaView style={styles.parentContainer}>
       <ScrollView>
-        <ScreenHeader screenHeaderTitle={"Browse favorites"} />
+        <View style={styles.childContainer}>
+          <ScreenHeader screenHeaderTitle={"Browse favorites"} />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
