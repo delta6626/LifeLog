44c16c4024ff313818f057d184e36dbfa095c55a
Delta6626 <hassan911ee@gmail.com>
2026-07-21 02:31:15 +0400

    MAJOR REFACTOR 2 - LOAD ENTRY DATA AND FIX SCROLLING ISSUE

--- src/app/entry.tsx
+++ src/app/entry.tsx
@@ -1,19 +1,34 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { KeyboardAvoidingView, StyleSheet, View } from "react-native";
 import { TextInput } from "react-native-paper";
 import { RichEditor, RichToolbar } from "react-native-pell-rich-editor";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScreenHeader } from "../../components/ScreenHeader";
+import { useCurrentEntryStore } from "../../store/currentEntryStore";
 import { useEntryScreenModeStore } from "../../store/entryScreenModeStore";
+import { getEntryFile } from "../../utils/crudHelpers";
 import { useAppTheme } from "../../utils/useAppTheme";
 
 export default function EntryScreen() {
   const theme = useAppTheme();
   const { entryScreenMode } = useEntryScreenModeStore();
+  const { currentEntryId } = useCurrentEntryStore();
 
   const [title, setTitle] = useState<string>("");
 
   const editorRef = useRef<RichEditor>(null);
+
+  useEffect(() => {
+    const initializeEntry = async () => {
+      if (!currentEntryId || !editorRef.current) return;
+      const entryDetails = await getEntryFile(currentEntryId);
+
+      setTitle(entryDetails.title);
+      editorRef.current.setContentHTML(entryDetails.content);
+    };
+
+    initializeEntry();
+  }, [currentEntryId, editorRef]);
 
   const styles = StyleSheet.create({
     parentContainer: {
@@ -76,6 +91,7 @@ export default function EntryScreen() {
           ref={editorRef}
           style={styles.editor}
           disabled={entryScreenMode === "read"}
+          useContainer={false}
         />
       </View>
 
